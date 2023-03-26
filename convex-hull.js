0e857690f1553be1d7388bae042f4e18a489f008
SebastienBrown <sebastienbrown1@gmail.com>
2023-03-26 15:01:24 -0400

    Fixed

---
+++ convex-hull.js
@@ -0,0 +1,181 @@
+const { getSystemErrorMap } = require("util");
+
+const SVG_NS = "http://www.w3.org/2000/svg";
+const SVG_WIDTH = 600;
+const SVG_HEIGHT = 400;
+
+// An object that represents a 2-d point, consisting of an
+// x-coordinate and a y-coordinate. The `compareTo` function
+// implements a comparison for sorting with respect to x-coordinates,
+// breaking ties by y-coordinate.
+function Point (x, y, id) {
+    this.x = x;
+    this.y = y;
+    this.id = id;
+
+    // Compare this Point to another Point p for the purposes of
+    // sorting a collection of points. The comparison is according to
+    // lexicographical ordering. That is, (x, y) < (x', y') if (1) x <
+    // x' or (2) x == x' and y < y'.
+    this.compareTo = function (p) {
+	if (this.x > p.x) {
+	    return 1;
+	}
+
+	if (this.x < p.x) {
+	    return -1;
+	}
+
+	if (this.y > p.y) {
+	    return 1;
+	}
+
+	if (this.y < p.y) {
+	    return -1;
+	}
+
+	return 0;
+    }
+
+    // return a string representation of this Point
+    this.toString = function () {
+	return "(" + x + ", " + y + ")";
+    }
+}
+
+// An object that represents a set of Points in the plane. The `sort`
+// function sorts the points according to the `Point.compareTo`
+// function. The `reverse` function reverses the order of the
+// points. The functions getXCoords and getYCoords return arrays
+// containing x-coordinates and y-coordinates (respectively) of the
+// points in the PointSet.
+function PointSet () {
+    this.points = [];
+    this.curPointID = 0;
+
+    // create a new Point with coordintes (x, y) and add it to this
+    // PointSet
+    this.addNewPoint = function (x, y) {
+	this.points.push(new Point(x, y, this.curPointID));
+	this.curPointID++;
+    }
+
+    // add an existing point to this PointSet
+    this.addPoint = function (pt) {
+	this.points.push(pt);
+    }
+
+    // sort the points in this.points 
+    this.sort = function () {
+	this.points.sort((a,b) => {return a.compareTo(b)});
+    }
+
+    // reverse the order of the points in this.points
+    this.reverse = function () {
+	this.points.reverse();
+    }
+
+    // return an array of the x-coordinates of points in this.points
+    this.getXCoords = function () {
+	let coords = [];
+	for (let pt of this.points) {
+	    coords.push(pt.x);
+	}
+
+	return coords;
+    }
+
+    // return an array of the y-coordinates of points in this.points
+    this.getYCoords = function () {
+	let coords = [];
+	for (pt of this.points) {
+	    coords.push(pt.y);
+	}
+
+	return coords;
+    }
+
+    // get the number of points 
+    this.size = function () {
+	return this.points.length;
+    }
+
+    // return a string representation of this PointSet
+    this.toString = function () {
+	let str = '[';
+	for (let pt of this.points) {
+	    str += pt + ', ';
+	}
+	str = str.slice(0,-2); 	// remove the trailing ', '
+	str += ']';
+
+	return str;
+    }
+}
+
+
+function ConvexHullViewer (svg, ps) {
+    this.svg = svg;  // a n svg object where the visualization is drawn
+    this.ps = ps;    // a point set of the points to be visualized
+
+    // COMPLETE THIS OBJECT
+}
+
+/*
+ * An object representing an instance of the convex hull problem. A ConvexHull stores a PointSet ps that stores the input points, and a ConvexHullViewer viewer that displays interactions between the ConvexHull computation and the 
+ */
+function ConvexHull (ps, viewer) {
+    this.ps = ps;          // a PointSet storing the input to the algorithm
+    this.viewer = viewer;  // a ConvexHullViewer for this visualization
+
+    // start a visualization of the Graham scan algorithm performed on ps
+    this.start = function () {
+
+	// COMPLETE THIS METHOD
+	
+    }
+
+    // perform a single step of the Graham scan algorithm performed on ps
+    this.step = function () {
+	
+	// COMPLETE THIS METHOD
+	
+    }
+
+    // Return a new PointSet consisting of the points along the convex
+    // hull of ps. This method should **not** perform any
+    // visualization. It should **only** return the convex hull of ps
+    // represented as a (new) PointSet. Specifically, the elements in
+    // the returned PointSet should be the vertices of the convex hull
+    // in clockwise order, starting from the left-most point, breaking
+    // ties by minimum y-value.
+    this.getConvexHull = function () {
+        //System.out.println(ps);
+        sorted=this.ps.sort();
+        let stack=[];
+        stack.push(sorted[0]);
+        stack.push(sorted[1]);
+        console.log(stack);
+        for(let i=2;i<sorted.size();i++){
+            const a = stack[i-2];
+            const b = stack[i-1];
+            const c=sorted[i];
+            if (stack.length==1){
+                stack.push(sorted[i]);
+            }
+            else{
+                let j=i-2;
+                while(()&&(stack.length>1)){
+                    stack.pop();
+                    j-=1;
+                    a=stack[j];
+                    b=stack[j+1];
+                }
+                stack.push(c);
+            }
+            console.log(stack);
+        }
+	// COMPLETE THIS METHOD
+	
+    }
+}
